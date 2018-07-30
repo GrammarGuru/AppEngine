62e2663a1f98a3b4c4be562bf7097b3feb9ffef8
sungodavi <sungodavi@gmail.com>
2018-07-29 19:44:36 -0500

    Added tests

---
+++ python/tests/main_test.py
@@ -0,0 +1,15 @@
+import webapp2
+import webtest
+import unittest
+import main
+
+
+class HomeTest(unittest.TestCase):
+    def setUp(self):
+        self.app = webtest.TestApp(main.app)
+
+    def testHelloWorldHandler(self):
+        response = self.app.get('/')
+        self.assertEqual(response.status_int, 200)
+        self.assertEqual(response.normal_body, 'Hello, World!')
+        self.assertEqual(response.content_type, 'text/plain')
